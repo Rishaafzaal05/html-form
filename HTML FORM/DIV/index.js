88e6a430c08a412718e59650a1e1bebe7d3ea839
Risha afzaal <afzaalrisha05@gmail.com>
2025-03-15 02:36:10 +0500

    Add files via upload

---
+++ HTML FORM/DIV/index.js
@@ -0,0 +1,21 @@
+alert ("hi risha")
+
+// data type
+
+alert("hi risha");
+alert(76373723);
+alert(true);
+alert(false);
+
+
+a = prompt("Enter your name")
+alert(a)
+document.write(a)
+
+// data type
+//" " string
+//number int 3839 
+//uint 12384820
+//bool true false
+
+
